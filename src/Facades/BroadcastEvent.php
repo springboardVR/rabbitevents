ca26e1c8b6d205b252a41db2621376af94d251ca
Nicholas Bates <nicholas@nrobates.com>
2020-03-18 10:52:12 -0700

    Adding New Testing Fake

--- src/Facades/BroadcastEvent.php
+++ src/Facades/BroadcastEvent.php
@@ -2,13 +2,53 @@
 
 namespace Nuwber\Events\Facades;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Facade;
+use Illuminate\Database\Eloquent\Model;
+use Nuwber\Support\Testing\RabbitEventFake;
 
 /**
  * @see \Nuwber\Events\Dispatcher
  */
 class BroadcastEvent extends Facade
 {
+    /**
+     * Replace the bound instance with a fake.
+     *
+     * @param  array|string  $eventsToFake
+     * @return \Illuminate\Support\Testing\Fakes\EventFake
+     */
+    public static function fake($eventsToFake = [])
+    {
+        static::swap($fake = new RabbitEventFake(static::getFacadeRoot(), $eventsToFake));
+
+        Model::setEventDispatcher($fake);
+        Cache::refreshEventDispatcher();
+
+        return $fake;
+    }
+
+    /**
+     * Replace the bound instance with a fake during the given callable's execution.
+     *
+     * @param  callable  $callable
+     * @param  array  $eventsToFake
+     * @return callable
+     */
+    public static function fakeFor(callable $callable, array $eventsToFake = [])
+    {
+        $originalDispatcher = static::getFacadeRoot();
+
+        static::fake($eventsToFake);
+
+        return tap($callable(), function () use ($originalDispatcher) {
+            static::swap($originalDispatcher);
+
+            Model::setEventDispatcher($originalDispatcher);
+            Cache::refreshEventDispatcher();
+        });
+    }
+
     /**
      * Get the registered name of the component.
      *
